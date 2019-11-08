28961c7de847b74c097abb549d7b336640a65b24
nicolas hilaire <nicolas.hilaire@carbonbee.fr>
2019-11-08 12:55:15 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/Constants.cpp
@@ -0,0 +1,5 @@
+#include "Arduino.h"
+
+#include "Constants.h"
+
+const String Constants::ApplicationVersion = "1.0.0+dev";
--- src/Constants.h
+++ src/Constants.h
@@ -1,6 +1,8 @@
+#include <Arduino.h>
 class Constants
 {
     public:
         static const int LedPin = 4;
         static const int PixelSize = 3;
+        static const String ApplicationVersion;
 };
