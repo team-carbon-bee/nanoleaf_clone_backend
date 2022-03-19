b526444624403da67c0db5b6b904d95dd0afe5a5
Yannick RICHARDOT <Yannick.richardot@carbonbee.fr>
2022-03-19 23:35:28 +0100

    Add icons for playing animation

--- data/animation.js
+++ data/animation.js
@@ -54,6 +54,16 @@ function clear_list_of_custom_animations() {
     }
 }
 
+function play_animation_item(child_els) {
+    const regex = new RegExp('([0-9]+) *-?');
+
+    let parent = child_els.closest(".list-group-item");
+    let match = parent.innerText.match(regex);
+    if (match) {
+        api_rest_preview_animation(match[1]);
+    }
+}
+
 function remove_animation_item(child_els) {
     child_els.closest(".list-group-item").remove();
 }
@@ -76,19 +86,35 @@ function clear_list_of_all_animations() {
     }
 }
 
-function add_trash_icons_to_animation_item(item) {
+function add_play_icons_to_animation_item(item) {
     // Create trash icons for remove it
-    let trash_svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    trash_svg.setAttribute("width", "24");
-    trash_svg.setAttribute("height", "24");
-    let trash_path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    trash_path.setAttribute("d", "M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z");
-    trash_path.setAttribute("class", "del_button");
-    trash_path.setAttribute("onclick", "remove_animation_item(this)");
-    trash_svg.appendChild(trash_path);
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("width", "28");
+    svg.setAttribute("height", "28");
+    let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", "m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z");
+    path.setAttribute("transform", "scale(1.8, 1.8)");
+    path.setAttribute("class", "play_icons");
+    path.setAttribute("onclick", "play_animation_item(this)");
+    svg.appendChild(path);
 
     // Add trash icons to item
-    item.lastChild.insertAdjacentElement("afterbegin", trash_svg);
+    item.lastChild.insertAdjacentElement("afterbegin", svg);
+}
+
+function add_trash_icons_to_animation_item(item) {
+    // Create trash icons for remove it
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("width", "28");
+    svg.setAttribute("height", "28");
+    let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", "M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z");
+    path.setAttribute("class", "del_button");
+    path.setAttribute("onclick", "remove_animation_item(this)");
+    svg.appendChild(path);
+
+    // Add trash icons to item
+    item.lastChild.insertAdjacentElement("afterbegin", svg);
 }
 
 function create_animation_item(animation_name) {
@@ -96,15 +122,15 @@ function create_animation_item(animation_name) {
     let icons = document.createElement("div");
 
     // Create handle icons for drag and drop
-    let handle_svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    handle_svg.setAttribute("class", "handle ms-2");
-    handle_svg.setAttribute("width", "24");
-    handle_svg.setAttribute("height", "24");
-    let handle_path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    handle_path.setAttribute("transform", "scale(1.9, 1.9)");
-    handle_path.setAttribute("d", "M2 8a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2z");
-    handle_svg.appendChild(handle_path);
-    icons.appendChild(handle_svg);
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("class", "handle ms-2");
+    svg.setAttribute("width", "28");
+    svg.setAttribute("height", "28");
+    let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("transform", "scale(1.8, 1.8)");
+    path.setAttribute("d", "M2 8a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2z");
+    svg.appendChild(path);
+    icons.appendChild(svg);
 
     // Create group item
     let item = document.createElement("div");
@@ -124,6 +150,9 @@ function append_animation_to_all_animations_list(animation_name) {
 
     // Create animation item
     let item = create_animation_item(animation_name);
+
+    // Add play icons
+    add_play_icons_to_animation_item(item);
 
     // Append item to list
     list.appendChild(item);
